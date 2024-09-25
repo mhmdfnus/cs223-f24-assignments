69e57a2a1de7364482fcecf581e4d9523aa22c5f
mhmdfnus <mfanous@haverford.edu>
2024-09-25 15:14:27 -0400

    test

--- A04/danceable.c
+++ A04/danceable.c
@@ -1,10 +1,11 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
+ * Author: Mohammad Fanous
+ * Date: 09/27/2024
  * Description
  ---------------------------------------------*/
 #include <stdio.h>
 
 int main() {
+  
   return 0;
 }
